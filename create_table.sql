c69ba7e45c122f747f41bdd5bcc9125c99ab7bc4
sganesh200594 <sreeb1443@gmail.com>
2024-09-25 09:07:46 +0530

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,1 +1,1 @@
-call sp_create_table_from_stage({'SCHEMA':'training', 'STAGE':'snowf_train_stage', 'TABLE_NAME':'house_pricing'})
+call sp_create_table_from_stage({'SCHEMA':'train', 'STAGE':'snowflake_train_stage', 'TABLE_NAME':'house_pricing'})
